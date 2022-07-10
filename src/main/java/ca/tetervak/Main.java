13c3566a78446eb01f0287bffbee152003dd8505
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-07-10 10:54:19 -0400

    initial commit

---
+++ src/main/java/ca/tetervak/Main.java
@@ -0,0 +1,10 @@
+package ca.tetervak;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World!");
+    }
+}
